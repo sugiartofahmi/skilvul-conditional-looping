5996dd0135e4cc4807dc7cdfb6eacd2fb51ac28a
sugiartofahmi <sugiartofahmi@gmail.com>
2023-03-31 12:25:34 +0700

    Fahmi Sugiarto | Complete exercise 1,2 & 3

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 //Exercise 1
-let exercise1 = "";
-for (let i = 1; i <= 100; i++) exercise1 += `<li>User ke-${i}</li>`;
-document.getElementById("exercise1").innerHTML = exercise1;
+for (let i = 1; i <= 100; i++)
+  document.getElementById("exercise1").innerHTML += `<h3>User ke-${i}</h3>`;
 
 //Exercise 2
-let exercise2 = "";
-for (let i = 0; i <= 10; i += 2) exercise2 += `<h3>Nilai = ${i}</h3>`;
-document.getElementById("exercise2").innerHTML = exercise2;
+for (let i = 0; i <= 10; i += 2)
+  document.getElementById("exercise2").innerHTML += `<h3>Nilai = ${i}</h3>`;
 
 //Exercise 3
-let exercise3 = "";
 for (let i = 0; i <= 20; i++)
-  exercise3 += `<h3>Angka ${i} = ${i % 2 == 0 ? "Genap" : "Ganjil"}</h3>`;
-document.getElementById("exercise3").innerHTML = exercise3;
+  document.getElementById("exercise3").innerHTML += `<h3>Angka ${i} = ${
+    i % 2 == 0 ? "Genap" : "Ganjil"
+  }</h3>`;
 
-window.confirm("Do you really want to leave?");
 //Exercise 4
